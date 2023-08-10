fe487490f01d26842cc3aa61e2ee2f4463acded3
Megan Glorioso <meganglorioso2016@gmail.com>
2023-08-09 20:16:25 -0700

    changed index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
